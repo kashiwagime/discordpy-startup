85f3c9e9465d84c0dcf3918d2fd972677365bf12
kashiwagime <51430421+kashiwagime@users.noreply.github.com>
2019-06-06 15:38:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,16 +13,19 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-@client.event
+@bot.event
 async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
+    try:
+        if message.author.bot:
+            return
+        await bot.process_commands(message)
+    except Exception:
+        await message.channel.send(f'```\n{traceback.format_exc()}\n```')
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
+@bot.command()
 
 
 bot.run(token)

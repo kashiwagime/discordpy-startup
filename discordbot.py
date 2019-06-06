a29e1ea76fbeb5e0c2ce7a574d5ed97be90f88f9
kashiwagime <51430421+kashiwagime@users.noreply.github.com>
2019-06-06 15:40:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,16 +2,9 @@ from discord.ext import commands
 import os
 import traceback
 
-import discord
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
 @bot.event
 async def on_message(message):
@@ -21,11 +14,11 @@ async def on_message(message):
         await bot.process_commands(message)
     except Exception:
         await message.channel.send(f'```\n{traceback.format_exc()}\n```')
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
 
 
-@bot.command()
+@bot.commands
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 bot.run(token)
